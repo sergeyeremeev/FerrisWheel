06b3b5b922af7dfc45df436d179aaabb93ea5181
Sergey Eremeev <13.eremeev.sv@gmail.com>
2017-04-23 02:00:59 +0300

    test items styles, more js setup - classes, data attributes and dimensions for wrapper element calculations

--- app/js/main.js
+++ app/js/main.js
@@ -24,9 +24,11 @@ var jQuery = require('jquery');
         this.element = element;
         this.options = extendHelper({}, FerrisWheel.defaults, options);
 
+        this._container = null;
         this._containerWidth = null;
         this._containerHeight = null;
         this._itemWidth = null;
+        this._itemHeight = null;
         this._items = [];
         this._controls = {
             dots: [],
@@ -54,13 +56,16 @@ var jQuery = require('jquery');
         // cache carousel items length to be used in loop
         itemsLength = this.element.children.length;
 
-        // store carousel items in this._items property
+        // store carousel items in this._items property, add class and data attr to each element
         for (i = 0; i < itemsLength; i++) {
             this._items.push(this.element.children[i]);
+            this._items[i].classList.add('ferris-item');
+            this._items[i].setAttribute('data-ferris-item', i);
         }
 
-        // get carousel items width and store in this._itemWidth property
+        // get carousel items width/height and store in this._itemWidth/this._itemHeight properties
         this._itemWidth = this._items[0].offsetWidth;
+        this._itemHeight = this._items[0].offsetHeight;
     };
 
     FerrisWheel.prototype.initialize = function () {
@@ -71,6 +76,10 @@ var jQuery = require('jquery');
         // wrap carousel items in wrapper div
         this.wrapItems();
 
+        // set container class based on the number of items and container dimensions
+        this.setContainerClass();
+        this.setContainerDimensions();
+
         // create carousel controls if navigation is set to true
         if (this.options.navigation) {
             this.createControls();
@@ -78,14 +87,39 @@ var jQuery = require('jquery');
     };
 
     FerrisWheel.prototype.wrapItems = function () {
+        var mainWrapper;
 
         // create carousel wrapper
-        this.mainWrapper = document.createElement('div');
-        this.mainWrapper.className = 'ferris-items';
+        mainWrapper = document.createElement('div');
+        mainWrapper.className = 'ferris-items';
 
         // wrap all items in created wrapper
         // TODO: add JS helper to substitute wrapAll()
-        $(this.element.children).wrapAll(this.mainWrapper);
+        $(this.element.children).wrapAll(mainWrapper);
+    };
+
+    FerrisWheel.prototype.setContainerClass = function () {
+        this._container = this.element.querySelector('.ferris-items');
+        this._container.className += ' ferris-items--' + this._items.length;
+    };
+
+    FerrisWheel.prototype.setContainerDimensions = function () {
+
+        // set container's height to 1.5 item height
+        var containerHeight = Math.round(this._itemHeight * 1.5);
+
+        // * Use paddings for centering to reserve transforms for actual animations
+
+        // set container's padding-top to half the space underneath the primary item
+        var containerPaddingTop = Math.round((containerHeight - this._itemHeight) / 3);
+
+        // center main item horizontally with padding-left
+        var containerPaddingLeft = Math.round((this._container.offsetWidth - this._itemWidth) / 2);
+
+        this._container.setAttribute('style', 'height: ' + containerHeight + 'px; '
+                                           + 'padding-top: ' + containerPaddingTop + 'px; '
+                                           + 'padding-left: ' + containerPaddingLeft + 'px; '
+        );
     };
 
     FerrisWheel.prototype.createControls = function () {
